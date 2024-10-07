da9ed49cc7e9b9d20e1795743fa778ee750fef22
Burak <burak.dunal@gmail.com>
2024-10-07 19:44:19 +0300

    section 4 game app init

---
+++ section4_app1/App.js
@@ -0,0 +1,44 @@
+import { ImageBackground, StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
+import GameScreen from "./screens/GameScreen";
+
+export default function App() {
+  const [userNumber, setUserNumber] = useState(null);
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if (userNumber) {
+    screen = <GameScreen />;
+  }
+  return (
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <SafeAreaView style={styles.screens}>{screen}</SafeAreaView>
+      </ImageBackground>
+    </LinearGradient>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  screens: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight || 0,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
