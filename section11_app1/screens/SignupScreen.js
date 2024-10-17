145e50e15a2e918b4877a5559385cd8ff3cc7bd3
burak.dunal <burak.dunal@etiya.com>
2024-10-17 15:23:29 +0300

    authenticating with Firebase

---
+++ section11_app1/screens/SignupScreen.js
@@ -0,0 +1,34 @@
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
+
+function SignupScreen() {
+  const [isAuthing, setIsAuthing] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
+  async function signupHandler({ email, password }) {
+    setIsAuthing(true);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not create user. Please check your input and try again later!"
+      );
+      setIsAuthing(false);
+    }
+  }
+
+  if (isAuthing) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
